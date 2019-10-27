a10b67d904cd5a488c0f015eaa40dd2246f50b61
william <818@holbertonschool.com>
2019-10-27 06:48:59 +0000

    [FEATURE] Add gui

---
+++ gui.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""the main gui component"""
+import graph
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+quest = QLabel('Which graph would you like to see?')
+
+delta = QPushButton('Delta')
+american = QPushButton('American Airlines')
+spirit = QPushButton('Spirit')
+jblue = QPushButton('jetBlue')
+
+jblue.clicked.connect(graph.graph_yelp)
+
+layout.addWidget(quest)
+layout.addWidget(jblue)
+layout.addWidget(delta)
+layout.addWidget(american)
+layout.addWidget(spirit)
+window.setLayout(layout)
+window.show()
+
+app.exec_()
